a44373f8201fa49f76b3747afa70a0528798256c
Athos Kele <unityguru23@gmail.com>
2018-02-18 12:56:48 +0200

    Add files via upload
    Uploaded GameObjectVariable for storing prefabs

---
+++ Scriptables/Variables/GameObjectVariable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SO
+{
+    /// <summary>
+    /// Similar to transform variables but this one is for keeping references to gameObjects or 
+    /// better yet, prefabs. So you don't waste memory allocating the same prefab on multiple objects.
+    /// </summary>
+    [CreateAssetMenu(menuName = "Variables/GameObject")]
+    public class GameObjectVariable : ScriptableObject
+    {
+        public GameObject value;
+    }
+}
