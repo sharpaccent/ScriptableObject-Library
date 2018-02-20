8cfef2d0b12827a56af6802d8c04bcb377e0dae2
Athos Kele <unityguru23@gmail.com>
2018-02-20 14:56:32 +0200

    Update GameEventListener.cs

--- Scriptables/Events/GameEventListener.cs
+++ Scriptables/Events/GameEventListener.cs
@@ -10,16 +10,32 @@ namespace SO
         public GameEvent gameEvent;
         public UnityEvent response;
 
+        /// <summary>
+        /// Override this to override the OnEnableLogic()
+        /// </summary>
+        public virtual void OnEnableLogic()
+        {
+            if (gameEvent != null)
+                gameEvent.Register(this);
+        }
+
         void OnEnable()
         {
-            if(gameEvent != null)
-                gameEvent.Register(this);
+            OnEnableLogic();
+        }
+
+        /// <summary>
+        /// Override this to override the OnDisableLogic()
+        /// </summary>
+        public virtual void OnDisableLogic()
+        {
+            if (gameEvent != null)
+                gameEvent.UnRegister(this);
         }
 
         void OnDisable()
         {
-            if (gameEvent != null)
-                gameEvent.UnRegister(this);
+            OnDisableLogic();
         }
 
         public virtual void Response()
