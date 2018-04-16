89d2de88b02f705e1d7e4a9361dd2cf58861d6bb
Athos Kele <unityguru23@gmail.com>
2018-04-16 11:01:58 +0300

    Script additions
    Added a helper script and a Vector3 variable

---
+++ Scriptables/Helpers/AssignTransform.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SO;
+
+namespace SA
+{
+    public class AssignTransform : MonoBehaviour
+    {
+        public TransformVariable transformVariable;
+
+		private void OnEnable()
+		{
+			transformVariable.value = this.transform;
+			Destroy(this);
+		}
+
+	}
+}
---
+++ Scriptables/Variables/Vector3Variable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SO
+{
+	[CreateAssetMenu(menuName = "Variables/Vector3")]
+	public class Vector3Variable : ScriptableObject
+	{
+		public Vector3 value;
+	}
+}
